7e81ec0c00905ab130ebb0b670838c7db5205eef
Artur <kourovartur@yandex.ru>
2022-08-21 01:03:19 +0300

    ToLower

---
+++ Example005/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("ВВедите имя пользователя: ");
+string username = Console.ReadLine();
+if(username.ToLower() == "маша")
+{
+    Console.WriteLine("Ура, это Маша!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
