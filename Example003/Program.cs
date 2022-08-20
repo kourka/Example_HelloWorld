75c934e2b364b74d62551d47403afac37221b31f
Artur <kourovartur@yandex.ru>
2022-08-21 00:28:30 +0300

    Random

--- Example003/Program.cs
+++ Example003/Program.cs
@@ -1,4 +1,6 @@
-﻿int number1 = 4;
-int number2 = 5;
+﻿int number1 = new Random().Next(1, 10); //1,2,3,4....10
+Console.WriteLine(number1);
+int number2 = new Random().Next(1, 10);
+Console.WriteLine(number2);
 int result = number1 + number2 ;
 Console.WriteLine (result);
