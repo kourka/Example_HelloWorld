bf7336fc09b6e31f53d3ccbc4b303ec898e3bc64
Artur <kourovartur@yandex.ru>
2022-08-27 19:07:51 +0300

    find

---
+++ Example010(2_lekcia)/Program.cs
@@ -0,0 +1,14 @@
+﻿int[] array = { 23, 453, 232, 11, 6, 231, 11 };
+int n = array.Length;
+int find = 231;
+int index = 0;
+
+while (index < n) 
+{
+    if (array[index] == find) 
+    {
+        Console.WriteLine(index);
+    }
+    index++;
+}
+
