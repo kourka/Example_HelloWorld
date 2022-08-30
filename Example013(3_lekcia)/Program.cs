263711de8adcebeb00f484507dfaeeda6ec6c3f2
Artur <kourovartur@yandex.ru>
2022-08-30 16:31:55 +0300

    Cikl_v_Cikle

---
+++ Example013(3_lekcia)/Program.cs
@@ -0,0 +1,25 @@
+﻿// // Цикл for
+
+
+// string Metod4(int count, string text)
+// {
+//     string result = String.Empty;
+//     for (int i = 0; i < count; i++)
+//     {
+//         result = result + text;
+//     }
+//     return result;
+// }
+// string res = Metod4(10, "asdf"); Console.WriteLine(res);
+
+// Цикл в цикле
+
+
+for (int i = 2; i <= 10; i++)
+{
+    for (int j = 2; j <= 10; j++)
+    {
+        Console.WriteLine($"{i} * {j} = {i * j}");
+    }
+    Console.WriteLine();
+}
