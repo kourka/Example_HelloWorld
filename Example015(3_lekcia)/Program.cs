d4183e9ed73e88c771c767a8dca5b9c3beb0f685
Artur <kourovartur@yandex.ru>
2022-08-30 17:27:49 +0300

    упорядочить_массивы

---
+++ Example015(3_lekcia)/Program.cs
@@ -0,0 +1,39 @@
+﻿// Упорядочить массивы
+
+// Было:68321457 
+// Стало:18326457
+
+int[] arr = { 1, 5, 4, 3, 2, 6, 7, 1, 1 };
+void PtintArray(int[] array)
+{
+    int count = array.Length;
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]}");
+    }
+    Console.WriteLine();
+}
+void SelectionSort(int[] array)
+{
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        int minPosition = i;
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            if (array[j] < array[minPosition]) minPosition = j;
+            {
+                minPosition = j;
+            }
+
+        }
+        int temporary = array[i];
+        array[i] = array[minPosition];
+        array[minPosition] = temporary;
+
+    }
+
+}
+
+PtintArray(arr);
+SelectionSort(arr);
+PtintArray(arr);
