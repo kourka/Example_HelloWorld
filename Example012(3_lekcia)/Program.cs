a0bf8d213b87de63d7f2939b8ab810b023894244
Artur <kourovartur@yandex.ru>
2022-08-30 16:18:08 +0300

    Metod_4

---
+++ Example012(3_lekcia)/Program.cs
@@ -0,0 +1,58 @@
+﻿// // Первая группа методов
+
+// void Metod1()
+// {
+// Console.WriteLine("Автор ..."); }
+// Metod1();
+
+
+
+
+// Вторая группа методов
+
+// void Metod2(string msg)
+// {
+//     Console.WriteLine(msg);
+
+// }
+// Metod2("Текст сообщения");
+
+
+// void Metod21(string msg, int count)
+// {
+//     int i =0;
+//     while (i < count) 
+//     {
+//             Console.WriteLine(msg);
+//             i++;
+//     }
+// }
+// Metod21("Текст", 4);
+
+// Третья группа методов
+
+// int Metod3()
+// {
+//     return DateTime.Now.Year;
+
+// }
+
+// int year = Metod3();
+// Console.WriteLine(year);
+
+
+//  Четвёртая группа методов
+
+string Metod4(int count, string text)
+{
+    int i = 0;
+    string result = String.Empty;
+    while (i < count)
+    {
+        result = result + text;
+        i++;
+    }
+    return result;
+}
+string res = Metod4(10, "z");
+Console.WriteLine(res);
